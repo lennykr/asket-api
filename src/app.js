e1304dd85322d45a8fad60c197724fce917e4d63
Lenny Kraaijenhof <hi@lenny.kr>
2019-11-30 17:38:58 +0100

    Add support for CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 global.moment = require('moment');
 moment.locale('nl-be');
@@ -14,8 +15,9 @@ const port = 8080;
 
 require('dotenv').config();
 
-app.use(express.json())
+app.use(cors());
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
